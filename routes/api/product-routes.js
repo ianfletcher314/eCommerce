9132938aafd4d20cc9e5d0dc2641040b3c4848ee
Ian Fletcher <ianfletcher314@gmail.com>
2021-07-08 13:02:42 -0500

    added delete route. product routes are all written now they must be tested

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -24,7 +24,7 @@ router.get('/', (req, res) => {
 });
 
 // get one product
-rrouter.get('/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
@@ -118,7 +118,22 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product was found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
