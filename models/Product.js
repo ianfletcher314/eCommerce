f9913c94210d2af5facbcafe60480de3d18ed2cb
Ian Fletcher <ianfletcher314@gmail.com>
2021-07-08 12:10:07 -0500

    looked at seed data to see what Product model was supposed to include and added in product name, price, stock, and category_id

--- models/Product.js
+++ models/Product.js
@@ -1,6 +1,4 @@
-// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-// import our database connection from config.js
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
@@ -9,7 +7,26 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true
+  
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
   },
   {
     sequelize,
